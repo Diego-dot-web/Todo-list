72cf4c5bd4ba01ee15be8bd7d32edc1713ce0404
Diego <134121369+Diego-dot-web@users.noreply.github.com>
2024-01-22 22:13:25 -0500

    Add Webpack plugin and add scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
